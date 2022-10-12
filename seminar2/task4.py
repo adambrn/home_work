33e43a9d66ab9855ca5a460741a949dfafb9bb99
Alexey Peremyshlennikov <adambrn@mail.ru>
2022-10-12 22:40:15 +0700

    errors fix

--- seminar2/task4.py
+++ seminar2/task4.py
@@ -1,6 +1,7 @@
 #Задайте список из N элементов, заполненных числами из промежутка [-N, N]. Найдите произведение элементов на указанных позициях. Позиции вводятся с клавиатуры .
+import random
 number = int(input('Введите число : '))
-list_n = list(range(-number,number + 1))
+list_n = [random.randint(-number,number) for _ in range(number)]
 print(list_n)
 positions = input('Введите позиции для умножения через пробел : ')
 positions_array = [int(n) for n in positions.split()]
