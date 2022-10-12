cd3c487b5886e2b6691c94b653de7f7114a2af5b
Alexey Peremyshlennikov <adambrn@mail.ru>
2022-10-12 22:40:22 +0700

    task 5 edit

--- seminar2/task5.py
+++ seminar2/task5.py
@@ -2,9 +2,6 @@
 import random
 list_num = [12, 5, 664, 63, 5, 73, 93, 127, 432, 64, 34]
 
-#list_shuffled = list_num[:]
-#random.shuffle(list_shuffled)
-
 list_shuffled = []
 list_tmp = list_num[:]
 while  list_tmp != []:
