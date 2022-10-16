1276b871e3f85a1c21198229878d490e229864c0
Alexey Peremyshlennikov <adambrn@mail.ru>
2022-10-16 22:02:24 +0700

    edit fibonacci 2

--- seminar3/task5.py
+++ seminar3/task5.py
@@ -3,9 +3,9 @@
 def fibonacci(n):
     if n < 0:
         if n == -1:
-            return -1
-        if n == -2:
             return 1
+        if n == -2:
+            return -1
         return fibonacci(n + 2) - fibonacci(n + 1)
     elif n == 0:
         return 0
